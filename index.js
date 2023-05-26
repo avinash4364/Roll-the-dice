6e48e708cfcb518fef76798c7b8fecbe11b38baa
Avinash Kumar Yadav <aky25199@gmail.com>
2023-05-26 21:56:31 +0530

    completed the remaining javascript for the roll the dice game and also added the dots on the dice rather than using different images, using CSS and JavaScript

--- index.js
+++ index.js
@@ -17,6 +17,41 @@ let playerOneCurrentScore = 0;
 let playerTwoCurrentScore = 0;
 let playerOneSelected = true;
 
+// top and left positions of all the dots displayed on the dice
+const dotPositions = {
+  1: [[50, 50]],
+  2: [
+    [80, 20],
+    [20, 80],
+  ],
+  3: [
+    [80, 20],
+    [50, 50],
+    [20, 80],
+  ],
+  4: [
+    [20, 20],
+    [80, 20],
+    [20, 80],
+    [80, 80],
+  ],
+  5: [
+    [20, 20],
+    [80, 20],
+    [50, 50],
+    [20, 80],
+    [80, 80],
+  ],
+  6: [
+    [20, 20],
+    [80, 20],
+    [20, 50],
+    [80, 50],
+    [20, 80],
+    [80, 80],
+  ],
+};
+
 // revert background
 const revert = function () {
   players.forEach((player) => {
@@ -24,11 +59,29 @@ const revert = function () {
   });
 };
 
+const createDot = function (dotNum) {
+  const dotPosition = dotPositions[dotNum];
+  for (let i = 0; i < dotNum; i++) {
+    const div = document.createElement('div');
+    div.classList.add('dot');
+    div.style.left = dotPosition[i][0] + '%';
+    div.style.top = dotPosition[i][1] + '%';
+    dice.appendChild(div);
+  }
+};
+
 // click event to the hold button
 rollBtn.addEventListener('click', () => {
   // a random number between 1 and 6
   diceNum = Math.trunc(Math.random() * 6 + 1);
   console.log(diceNum);
+
+  // check if dots are already present and then remove them
+  //   we can set the innerHTML of the parent element to empty
+  dice.innerHTML = '';
+
+  //   create the dot according to the random number generated
+  createDot(diceNum);
 
   // show the dice
   dice.classList.add('show');
@@ -71,4 +124,22 @@ holdBtn.addEventListener('click', () => {
 
   // revert the background
   revert();
+});
+
+// reset the game
+newBtn.addEventListener('click', () => {
+  playerOneScore = 0;
+  playerTwoScore = 0;
+  playerOneCurrentScore = 0;
+  playerTwoCurrentScore = 0;
+  playerOneScoreDisplay.textContent = playerOneScore;
+  playerTwoScoreDisplay.textContent = playerTwoScore;
+  playerOneCurrent.textContent = playerOneCurrentScore;
+  playerTwoCurrent.textContent = playerTwoCurrentScore;
+  dice.classList.remove('show');
+  dice.innerHTML = '';
+  if (!playerOneSelected) {
+    revert();
+    playerOneSelected = true;
+  }
 });
